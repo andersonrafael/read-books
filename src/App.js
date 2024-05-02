63743c31a98e46707f7760eaed2b7a30e57a3a80
anderson rafael <franciscoandersonrafael@gmail.com>
2024-05-01 21:08:19 -0300

    estilizando o header

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
+        <ul className='opcoes'>
+          <li> Categorias</li>
+          <li> Minha Estante</li>
+          <li> Favoritos</li>
+        </ul>
       </header>
     </div>
   );
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,10 +1,10 @@
-import './style.css'
-import logo from '../../assets/logo.svg'
+import './estilo.css';
+import logo from '../../assets/logo-livro.png'
 
 function Logo() {
     return (<div className='logo'>
-        <img src={logo} alt='logo'></img>
-        <p> <strong>Nerd</strong>BooKs</p>
+        <img src={logo} alt='logo' className='logo-img'></img>
+        <p> <strong>NerdBooKs</strong></p>
     </div>)
 }
 export default Logo
