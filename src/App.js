5418e63ca6660846e9c17af32c2b4eabfee10727
anderson rafael <franciscoandersonrafael@gmail.com>
2024-05-05 22:26:14 -0300

    botoes estilização

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@
 import './App.css';
 import Logo from './componentes/Logo'
 
+
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
         <ul className='opcoes'>
-          <li> Categorias</li>
-          <li> Minha Estante</li>
-          <li> Favoritos</li>
+          <li className='opcao1'> Categorias</li>
+          <li className='opcao2'> Minha Estante</li>
+          <li className='opcao3'> Favoritos</li>
         </ul>
       </header>
     </div>
